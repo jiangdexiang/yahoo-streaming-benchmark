eb45b414f1dd31d6ac35ca8a3d3765765135c510
蒋德祥(26020410) <jiangdexiang002@ke.com>
2019-09-05 14:48:38 +0800

    初始化项目

---
+++ streaming-dting-test/src/main/java/com/lianjia/bigdata/dataarch/streamingdtingtest/StreamingDtingTestApplication.java
@@ -0,0 +1,13 @@
+package com.lianjia.bigdata.dataarch.streamingdtingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingDtingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingDtingTestApplication.class, args);
+    }
+
+}
