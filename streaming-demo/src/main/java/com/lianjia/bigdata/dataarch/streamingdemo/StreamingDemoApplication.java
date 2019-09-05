4188f3d34e4c1e82a6f2daabab13f9c901e942dd
蒋德祥(26020410) <jiangdexiang002@ke.com>
2019-09-05 14:52:17 +0800

    init

---
+++ streaming-demo/src/main/java/com/lianjia/bigdata/dataarch/streamingdemo/StreamingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lianjia.bigdata.dataarch.streamingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingDemoApplication.class, args);
+    }
+
+}
